5bd0c37a3ccb12d369093cccafc046c8c6793266
Shujan Shaikh <shaikhshujan@gmail.com>
2025-10-23 16:18:43 +0530

    fixing th ebuild issue

--- apps/web/components/theme-provider.tsx
+++ apps/web/components/theme-provider.tsx
@@ -2,10 +2,11 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
